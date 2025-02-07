efb29a491a9d39acebc72c6da67c6de62f9fc034
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2025-02-07 19:26:04 -0300

    modularization of the project and creation of the "JsonDirectory" path

---
+++ Services/HtmlSelenium.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Webcrawler.Services
+{
+    public class HtmlSelenium
+    {
+        public static string LoadPageSelenium(string url)
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+            chromeOptions.AddArgument("--disable-gpu");
+            chromeOptions.AddArgument("--no-sandbox");
+
+            using (var driver = new ChromeDriver(chromeOptions))
+            {
+                driver.Navigate().GoToUrl(url);
+
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(driver =>
+                {
+                    var portElements = driver.FindElements(By.CssSelector("td span.port"));
+                    return portElements.Any(e => !string.IsNullOrWhiteSpace(e.Text));
+                });
+
+                return driver.PageSource;
+            }
+        } 
+    }
+}
