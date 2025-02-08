84e38772916fc74f4dcd4904b98de1b970820564
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2025-02-08 11:55:23 -0300

    feat: counting pages and saving to database

---
+++ Database/DatabaseService.cs
@@ -0,0 +1,35 @@
+﻿using MySqlConnector;
+using Webcrawler.Models;
+
+namespace Webcrawler.Database
+{
+    public class DatabaseService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void SaveExecutionInfo(SaveInfos info)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Open();
+
+            string query = @"
+                INSERT INTO SaveInfos (StartTime, EndTime, TotalPages, TotalRowsExtracted, JsonFileName)
+                VALUES (@StartTime, @EndTime, @TotalPages, @TotalRowsExtracted, @JsonFileName)";
+
+            using var command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@StartTime", info.StartTime);
+            command.Parameters.AddWithValue("@EndTime", info.EndTime);
+            command.Parameters.AddWithValue("@TotalPages", info.TotalPages);
+            command.Parameters.AddWithValue("@TotalRowsExtracted", info.TotalRowsExtracted);
+            command.Parameters.AddWithValue("@JsonFileName", info.JsonFileName);
+
+            command.ExecuteNonQuery();
+            Console.WriteLine("Dados de execução salvos no banco.");
+        }
+    }
+}
---
+++ Helpers/ConfigurationLoader.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+namespace Webcrawler.Helpers
+{
+    public static class ConfigurationLoader
+    {
+        public static IConfiguration Load() => new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+    }
+}
