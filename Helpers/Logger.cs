edab7241a0e8a3017f2810e8e58ea5ff4456e446
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2025-02-10 04:33:41 -0300

    feat: adding log

---
+++ Helpers/Logger.cs
@@ -0,0 +1,13 @@
+﻿namespace Webcrawler.Helpers
+{
+    internal class Logger
+    {
+        public static void Log(string message)
+        {
+            string logFilePath = $"Logs/log_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+            Directory.CreateDirectory(Path.GetDirectoryName(logFilePath)); 
+            File.AppendAllText(logFilePath, $"{DateTime.Now}: {message}{Environment.NewLine}");
+        }
+
+    }
+}
