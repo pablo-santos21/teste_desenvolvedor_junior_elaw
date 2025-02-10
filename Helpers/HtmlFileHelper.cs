8b43a685ff4f34aaaae2ddf872251c3887d984b4
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2025-02-10 03:05:53 -0300

    feat: configuring Multithread

---
+++ Helpers/HtmlFileHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace Webcrawler.Helpers
+{
+    public class HtmlFileHelper
+    {
+        public static void SavePageHtml(List<string> pageContents)
+        {
+            string directoryPath = "PagesDirectory";
+
+            if (Directory.Exists(directoryPath))
+            {
+                Directory.Delete(directoryPath, true);
+            }
+
+            Directory.CreateDirectory(directoryPath);
+
+            for (int i = 0; i < pageContents.Count; i++)
+            {
+                string filePath = Path.Combine(directoryPath, $"page-{i + 1}.html");
+                File.WriteAllText(filePath, pageContents[i]);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,38 +1,37 @@
-﻿    using Webcrawler.Helpers;
-    using Webcrawler.Services;
-    using Webcrawler.Database;
-    using Webcrawler.Models;
-    using Microsoft.Extensions.Configuration;
-using HtmlAgilityPack;
-using System;
+﻿using Webcrawler.Helpers;
+using Webcrawler.Services;
+using Webcrawler.Database;
+using Webcrawler.Models;
+using Microsoft.Extensions.Configuration;
 
-    var config = ConfigurationLoader.Load();
-    string connectionString = config.GetConnectionString("DefaultConnection");
+var config = ConfigurationLoader.Load();
+string connectionString = config.GetConnectionString("DefaultConnection");
+int MaxConcurrentTasks = int.Parse(config.GetSection("ScraperConfig:MaxDegreeOfParallelism").Value);
+string BaseUrl = config.GetSection("ScraperConfig:BaseUrl").Value;
 
-    var singlePageScraper = new SinglePageScraper();
-    var totalPage = new TotalPage();
-    var scraper = new ProxyScraper(singlePageScraper, totalPage);
-    var dbService = new DatabaseService(connectionString);
+var singlePageScraper = new SinglePageScraper();
+var totalPage = new TotalPage();
+var scraper = new ProxyScraper(singlePageScraper, totalPage, MaxConcurrentTasks);
+var dbService = new DatabaseService(connectionString);
 
-    DateTime startTime = DateTime.Now;
+DateTime startTime = DateTime.Now;
 
-    var url = "https://proxyservers.pro/proxy/list/order/updated/order_dir/desc";
-    var (proxies, totalPages) = scraper.GetProxiesFromPage(url);
+var (proxies, totalPages) = await scraper.GetProxiesFromPage(BaseUrl);
 
-    FileHelper.SaveProxiesToJson(proxies);
+ProxyFileHelper.SaveProxiesToJson(proxies);
 
-    DateTime endTime = DateTime.Now;
-    int totalRows = proxies.Count;
+DateTime endTime = DateTime.Now;
+int totalRows = proxies.Count;
 
-    var saveInfos = new SaveInfos
-    {
-        StartTime = startTime,
-        EndTime = endTime,
-        TotalPages = totalPages,
-        TotalRowsExtracted = totalRows,
-        JsonFileName = FileHelper.LastSavedJsonFileName
-    };
+var saveInfos = new SaveInfos
+{
+    StartTime = startTime,
+    EndTime = endTime,
+    TotalPages = totalPages,
+    TotalRowsExtracted = totalRows,
+    JsonFileName = ProxyFileHelper.LastSavedJsonFileName
+};
 
-    dbService.SaveExecutionInfo(saveInfos);
+dbService.SaveExecutionInfo(saveInfos);
 
-    Console.WriteLine("Scraping concluído e dados salvos.");
+Console.WriteLine("Scraping concluído e dados salvos.");
